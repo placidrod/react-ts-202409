e273c49952078b9da273e32c3d44ab53b9cff78d
Placid Rodrigues <placidrod@gmail.com>
2024-09-26 15:13:17 +1200

    Headless ui: ComboBox in Dialog

---
+++ src/App.tsx
@@ -0,0 +1,133 @@
+'use client';
+
+import { useState } from 'react';
+import {
+  Dialog,
+  DialogBackdrop,
+  DialogPanel,
+  DialogTitle,
+} from '@headlessui/react';
+// import { CheckIcon } from '@heroicons/react/24/outline'
+
+import {
+  Combobox,
+  ComboboxButton,
+  ComboboxInput,
+  ComboboxOption,
+  ComboboxOptions,
+} from '@headlessui/react';
+import { CheckIcon, ChevronDownIcon } from '@heroicons/react/20/solid';
+import clsx from 'clsx';
+
+const people = [
+  { id: 1, name: 'Tom Cook' },
+  { id: 2, name: 'Wade Cooper' },
+  { id: 3, name: 'Tanya Fox' },
+  { id: 4, name: 'Arlene Mccoy' },
+  { id: 5, name: 'Devon Webb' },
+];
+
+function CBox() {
+  const [query, setQuery] = useState('');
+  const [selected, setSelected] = useState(people[1]);
+
+  const filteredPeople =
+    query === ''
+      ? people
+      : people.filter((person) => {
+          return person.name.toLowerCase().includes(query.toLowerCase());
+        });
+
+  return (
+    <div className="mx-auto w-52 pt-20 z-10">
+      <Combobox
+        value={selected}
+        onChange={(value) => setSelected(value)}
+        onClose={() => setQuery('')}
+        __demoMode
+      >
+        <div className="relative">
+          <ComboboxInput
+            className={clsx(
+              'w-full rounded-lg border-none bg-black/5 py-1.5 pr-8 pl-3 text-sm/6 text-black',
+              'focus:outline-none data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-black/25'
+            )}
+            displayValue={(person) => person?.name}
+            onChange={(event) => setQuery(event.target.value)}
+          />
+          <ComboboxButton className="group absolute inset-y-0 right-0 px-2.5">
+            <ChevronDownIcon className="size-4 fill-black/60 group-data-[hover]:fill-black" />
+          </ComboboxButton>
+        </div>
+
+        <ComboboxOptions
+          anchor="bottom"
+          transition
+          className={clsx(
+            'w-[var(--input-width)] rounded-xl border border-black/5  p-1 [--anchor-gap:var(--spacing-1)] empty:invisible bg-white z-10',
+            'transition duration-100 ease-in data-[leave]:data-[closed]:opacity-0'
+          )}
+        >
+          {filteredPeople.map((person) => (
+            <ComboboxOption
+              key={person.id}
+              value={person}
+              className="group flex cursor-default items-center gap-2 rounded-lg py-1.5 px-3 select-none data-[focus]:bg-gray-400"
+            >
+              <CheckIcon className="invisible size-4 fill-black group-data-[selected]:visible" />
+              <div className="text-sm/6 text-black">{person.name}</div>
+            </ComboboxOption>
+          ))}
+        </ComboboxOptions>
+      </Combobox>
+    </div>
+  );
+}
+
+export default function Modal() {
+  const [open, setOpen] = useState(true);
+
+  return (
+    <Dialog open={open} onClose={setOpen} className="relative z-10">
+      <DialogBackdrop
+        transition
+        className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in"
+      />
+
+      <div className="fixed inset-0 w-screen overflow-y-auto">
+        <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+          <DialogPanel
+            transition
+            className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all data-[closed]:translate-y-4 data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in sm:my-8 sm:w-full sm:max-w-sm sm:p-6 data-[closed]:sm:translate-y-0 data-[closed]:sm:scale-95"
+          >
+            <div>
+              {/* <div className="mx-auto flex h-12 w-12 items-center justify-center rounded-full bg-green-100">
+                <CheckIcon aria-hidden="true" className="h-6 w-6 text-green-600" />
+              </div> */}
+              <div className="">
+                {/* <DialogTitle
+                  as="h3"
+                  className="text-base font-semibold leading-6 text-gray-900"
+                >
+                  Payment successful
+                </DialogTitle> */}
+                <div className="mt-2">
+                  <CBox />
+                </div>
+              </div>
+            </div>
+            <div className="mt-5 sm:mt-6">
+              <button
+                type="button"
+                onClick={() => setOpen(false)}
+                className="inline-flex w-full justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Go back to dashboard
+              </button>
+            </div>
+          </DialogPanel>
+        </div>
+      </div>
+    </Dialog>
+  );
+}
